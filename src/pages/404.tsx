f4a120e76743e4ba1c9822e18190ba8c2e6af92b
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-19 01:17:44 +0200

    remove old imports

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,5 @@
 import { type NextPage } from "next";
-import Image from "next/image";
 import Link from "next/link";
-import { Hero } from "../components/Hero";
 
 const PageNotFound: NextPage = () => {
   return (
