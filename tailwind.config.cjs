811b9f6d25293f1802b22d85f46c7f7abfd9c110
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-08 03:26:49 +0200

    feat: astro port

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.tsx",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
   daisyui: {
-    themes: [
-      "night",
-      "emerald",
-    ],
+    themes: ["night", "emerald"],
   },
   plugins: [require("daisyui")],
-}
+};
