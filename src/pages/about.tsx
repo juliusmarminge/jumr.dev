1e1fc41ad07601682cd32d1ebea777f526e5805f
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-12 13:44:55 +0200

    update sponsor button

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { AiOutlineHeart } from "react-icons/ai";
+import { NextLink } from "../components/next-link";
 
 const AboutPage: NextPage = () => {
   return (
@@ -8,18 +10,18 @@ const AboutPage: NextPage = () => {
         <title>About Me</title>
       </Head>
       <div className="p-8 card roundex-box bg-base-200">
-        <h1 className="text-3xl font-bold">
-          About
-        </h1>
+        <h1 className="text-3xl font-bold">About</h1>
         <p className="py-2">
-          I am a Software Engineer and open source contributor living in Sweden. If you would like to support my work, I accept donations on GitHub Sponsor.
+          I am a Software Engineer and open source contributor living in Sweden. If
+          you would like to support my work, I accept donations on GitHub Sponsor.
         </p>
-        <iframe
-          src="https://github.com/sponsors/juliusmarminge/button"
-          title="Sponsor juliusmarminge"
-          height="35"
-          width="116"
-        ></iframe>
+        <NextLink
+          href="https://github.com/sponsors/juliusmarminge"
+          className="btn w-48 gap-2 normal-case group rounded-lg btn-accent"
+        >
+          <AiOutlineHeart className="text-lg group-hover:text-pink-500 group-hover:scale-125 transition-transform" />
+          Sponsor me
+        </NextLink>
       </div>
     </>
   );
