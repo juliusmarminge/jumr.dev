6544b9bbab18d933fec73d5e80c30acfca5f08db
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-15 13:39:31 +0200

    eslint prettier setup

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,3 +1,11 @@
+/** @type {import('prettier').Config} */
 module.exports = {
   plugins: [require("prettier-plugin-tailwindcss")],
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
 };
