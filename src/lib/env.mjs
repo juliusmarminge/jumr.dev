774d88645b5fda0c3e17da2b74e7a63d1feea5ca
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-03 20:36:30 +0100

    schema fix

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -3,19 +3,18 @@ import { z } from "zod";
 const envSchema = z.object({
   GITHUB_ID: z.string(),
   GITHUB_SECRET: z.string(),
-  NEXTAUTH_URL: z
-    .string()
-    .transform((str) =>
+  NEXTAUTH_URL: z.preprocess(
+    (str) =>
       process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : str,
-    )
-    .pipe(z.string().url()),
+    z.string().url(),
+  ),
   NEXTAUTH_SECRET:
     process.env.NODE_ENV === "production" ? z.string() : z.string().optional(),
 });
 
 const _env = envSchema.safeParse(process.env);
 if (!_env.success) {
-  throw new Error("❌ Invalid environment variables", _env.error);
+  throw new Error("❌ Invalid environment variables", _env.error.flatten());
 }
 
 export const env = _env.data;
