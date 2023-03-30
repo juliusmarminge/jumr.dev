ec60bf1766c6784a8602aaf53e3f774569b49075
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-30 23:17:22 +0200

    tw 3.3 with ts config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { fontFamily } = require("tailwindcss/defaultTheme");
+import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./app/**/*.{tsx,mdx}",
     "./mdx-components.tsx",
@@ -29,5 +28,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar"), require("@tailwindcss/line-clamp")],
-};
+  plugins: [require("tailwind-scrollbar")],
+} satisfies Config;
